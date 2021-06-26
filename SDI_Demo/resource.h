9f7d4bf0e3919ad36e1f033a78eaff0a00a90d59
Yash Bhensdadiya <patelyashp333@gmail.com>
2021-06-26 16:11:06 +0530

    Single Document Interface

---
+++ SDI_Demo/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SDI_Demo.rc
+//
+#define IDR_MENU1                       101
+#define IDR_MAINFRAME                   101
+#define IDI_ICON2                       104
+#define ID_FILE_NEW                     40001
+#define ID_FILE_OPEN                    40002
+#define ID_FILE_SAVE                    40003
+#define ID_FILE_EXIT                    40004
+#define ID_FILE_RECENTFILES             40005
+#define ID_HELP                         40006
+#define ID_APP_EXIT                     40007
+#define ID_FILE_PRINTSETUP              40008
+#define ID_FILE_PRINT_SETUP             40009
+#define ID_FILE_                        40010
+#define ID_FILE_SAVEAS                  40011
+#define ID_FILE_SAVE_AS                 40012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40013
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
